e2a78c4c6974187eb3c144d0ce91feb2f21327de
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-01-30 10:04:53 -0400

    exercicios da aula 11

---
+++ EXERCICIOS/aula11/ex001.js
@@ -0,0 +1,9 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel >= 30) {
+    console.log(`Você foi multado! O limite de velocidade era 30 Km/h e você estáva a: ${vel} Km/h`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
