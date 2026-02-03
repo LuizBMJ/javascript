3736dabe0d9dd78d0e32a244cafdf396eed0b770
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-02-03 15:22:09 -0400

    Fiz o ultimo exercicio do curso

---
+++ EXERCICIOS/Modulo F/aula17/ex001.js
@@ -0,0 +1,12 @@
+
+let amigo = {nome: "jose",
+    sexo: "M",
+    peso: 95.4,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+
+amigo.engordar(2)
+console.log(amigo.peso)
